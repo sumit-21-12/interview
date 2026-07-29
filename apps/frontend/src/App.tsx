edc0ce05ed0350736179c290767f4d42e83d0a53
Sumit Singh <ss3864696@gmail.com>
2026-07-29 23:49:45 +0530

    added

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
       <Button>Click me</Button>
+      <input type="text" />
+      <input type="text" />
     </div>
   )
 }
